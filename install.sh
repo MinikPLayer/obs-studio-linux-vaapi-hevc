0537f806036b6aaf46090f9f438992303b27b685
M. Tomecki <39161575+MinikPLayer@users.noreply.github.com>
2022-06-30 03:22:33 +0200

    Added required submodules

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Use patch
+echo "Patching..."
 patch -p1 < ./hevc-vaapi.diff
 
 # Configure git submodules
+echo "Configuring git..."
 git config submodule.plugins/obs-outputs/ftl-sdk.url https://github.com/Mixer/ftl-sdk.git
 git config submodule.plugins/obs-browser.url https://github.com/obsproject/obs-browser.git
 git config submodule.plugins/obs-vst.url https://github.com/obsproject/obs-vst.git
 git submodule update
 
 # Create build files
+echo "Creating build files..."
 cmake -DCMAKE_INSTALL_PREFIX=/usr \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DBUILD_BROWSER=ON \
@@ -17,7 +20,9 @@ cmake -DCMAKE_INSTALL_PREFIX=/usr \
     -B build
 
 # Build project
+echo "Building..."
 cmake --build build
 
 # Install project
+echo "Installing..."
 sudo cmake --install build
